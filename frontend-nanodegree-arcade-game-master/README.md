73a4e0d7f0efdba745ced44a3a814fcbf36fb633
Dwaine Simmons <dwainesimmons88@gmail.com>
2018-07-31 18:55:57 -0400

    updated readme

---
+++ frontend-nanodegree-arcade-game-master/README.md
@@ -0,0 +1,20 @@
+frontend-nanodegree-arcade-game
+===============================
+
+Intro
+=================================
+
+This is a game about a cute little princess trying to making it to the river.
+
+
+How to play
+==================================
+
+Move the princess by using the arrow keys on the keyboard. You beat the game when you get to the river and avoid the bugs. The game will reset if you want to play again. 
+
+Contributions
+===================================
+Big thanks to Matthew Cranford for the help on some of the math and logic for the code.
+https://matthewcranford.com/arcade-game-walkthrough-part-1-starter-code-breakdown/
+
+Also big thanks to the FEND slack for keeping me motivated and finishing this project.
---
+++ frontend-nanodegree-arcade-game-master/js/app.js
@@ -0,0 +1,146 @@
+// Enemies our player must avoid
+var Enemy = function(x, y, speed) {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.x = x;
+    this.y = y + 55; // center
+    this.speed = speed;
+    this.sprite = 'images/enemy-bug.png';
+    this.step = 101;
+    this.boundary = this.step * 5;
+    this.resetPos = -this.step;
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+
+    //if enemy is not passed boundary
+      if(this.x < this.boundary) {
+      //move forward
+      //increment x by speed * dt
+      this.x += this.speed * dt;
+    }
+      else {
+        //reset pos to start
+        this.x = this.resetPos;
+      }
+};
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+class Hero {
+  constructor() {
+    this.sprite = 'images/char-princess-girl.png';
+    // how the character moves across the x axis
+    this.step = 101;
+    // how the character moves across the y axis
+    this.jump = 83;
+    // to center the character at the beginning of the game
+    this.startX = this.step * 2;
+    this.startY = (this.jump * 4) + /* Center*/ 55;
+    this.x = this.startX;
+    this.y = this.startY;
+    // sets victory as false
+    this.victory = false;
+}
+
+  reset() {
+    this.x = this.startX;
+    this.y = this.startY;
+  }
+
+// use the same render codes as the one for the bugs 
+  render() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+
+  }
+
+// big help from Matthew Cranford blog : https://matthewcranford.com/arcade-game-walkthrough-part-4-heros-first-steps/
+
+  handleInput(input) {
+    switch(input) {
+      case 'left':
+        if (this.x > 0) {
+        this.x -= this.step;
+      }
+        break;
+      case 'up':
+        if (this.y > 0) {
+        this.y -= this.jump;
+      }
+        break;
+      case 'right':
+        if (this.x < this.step * 4) {
+        this.x += this.step;
+      }
+        break;
+      case 'down':
+        if (this.y < this.jump * 4) {
+        this.y += this.jump;
+      }
+        break;
+    }
+  }
+
+  update() {
+    // Check collision here
+    for (let enemy of allEnemies) {
+      // Did player x and y collide with enemy?
+    if (this.y === enemy.y && (enemy.x + enemy.step > this.x && enemy.x < this.x + this.step) ) {
+      this.reset();
+    }
+      // Check win here?
+if (this.y === -28) {
+  // Did player x and reach final tile?
+  this.victory = true;
+  }
+
+}
+}
+
+
+  }
+
+
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
+
+const player = new Hero();
+const bug1 = new Enemy(-101, 0, 200);
+const bug2 = new Enemy(-101, 83, 300);
+const bug3 = new Enemy((-101*2.5), 83, 300);
+const bug4 = new Enemy((-101*2.5), 0, 300);
+const bug5 = new Enemy(-101, 0, 100);
+const allEnemies = [];
+allEnemies.push(bug1,bug2,bug3,bug4,bug5);
+console.log(allEnemies);
+
+
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
